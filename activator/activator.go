22e19be47b9a6fd000be2e23118a68ccc5b6157d
btracey <bdtinternet@gmail.com>
2013-10-31 18:13:16 -0700

    Removed marshaling stuff from activator

--- activator/activator.go
+++ activator/activator.go
@@ -15,50 +15,6 @@ func init() {
 	gob.Register(LinearTanh{})
 }
 
-// prefix is for marshalling and unmarshalling. The
-var prefix string = "github.com/btracey/nnet/activator/"
-
-//
-type marshalName struct{ Name string }
-
-// MarshalText marshalls the activators in this package for use with a
-// TextMarshaller. If the activator is not from this package, a
-// NotInPackage error will be returned
-func MarshalJSON(a Activator) ([]byte, error) {
-	// New types added with this package should be added here.
-	// Types should use prefix while marshalling
-	switch a.(type) {
-	default:
-		return nil, common.NotInPackage
-	case Sigmoid, Linear, Tanh, LinearTanh:
-		t := a.(json.Marshaler)
-		return t.MarshalJSON()
-	}
-}
-
-// UnmarshalText returns a package activator from the string.
-// If the string does not match any of the types in the package,
-// then a "NotInPackage" error is returned
-func UnmarshalJSON(b []byte) (Activator, error) {
-
-	name := &marshalName{}
-	json.Unmarshal(b, name)
-	switch name.Name {
-	case sigmoidMarshalString:
-		return Sigmoid{}, nil
-	case linearMarshalString:
-		return Linear{}, nil
-	case tanhMarshalString:
-		return Tanh{}, nil
-	case linearTanhMarshalString:
-		return LinearTanh{}, nil
-	default:
-		return nil, common.NotInPackage
-	}
-}
-
-// A set of built-in Activator types
-
 // Activator is an interface for the activation function of the neuron,
 // (allowing for neurons with custom activation functions).
 // An activator has two methods.
@@ -78,9 +34,6 @@ type Activator interface {
 // out = 1/(1 + exp(-sum))
 type Sigmoid struct{}
 
-// Here so that if changed, it is in one place here and in the Unmarshal code
-var sigmoidString string = "Sigmoid"
-
 // Activate computes the sigmoid activation function
 func (a Sigmoid) Activate(sum float64) float64 {
 	return 1.0 / (1.0 + math.Exp(-sum))
@@ -90,30 +43,6 @@ func (a Sigmoid) Activate(sum float64) float64 {
 // with respect to the weighted sum
 func (n Sigmoid) DActivateDCombination(sum, output float64) float64 {
 	return output * (1 - output)
-}
-
-/*
-func (a Sigmoid) String() string {
-	return sigmoidString
-}
-*/
-
-var sigmoidMarshalString string = prefix + sigmoidString
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a Sigmoid) MarshalJSON() ([]byte, error) {
-	return json.Marshal(marshalName{Name: sigmoidMarshalString})
-}
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a *Sigmoid) UnmarshalJSON(input []byte) error {
-	s := &marshalName{}
-	json.Unmarshal(input, &s)
-	if s.Name != sigmoidMarshalString {
-		return common.UnmarshalMismatch{Expected: sigmoidMarshalString, Received: s.Name}
-	}
-	a = &Sigmoid{}
-	return nil
 }
 
 // Linear neuron has a the identity activation function out = sum
@@ -187,24 +116,6 @@ func (a Tanh) String() string {
 	return tanhString
 }
 
-var tanhMarshalString string = prefix + tanhString
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a Tanh) MarshalJSON() ([]byte, error) {
-	return json.Marshal(marshalName{Name: tanhMarshalString})
-}
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a *Tanh) UnmarshalJSON(input []byte) error {
-	s := &marshalName{}
-	json.Unmarshal(input, &s)
-	if s.Name != tanhMarshalString {
-		return common.UnmarshalMismatch{Expected: tanhMarshalString, Received: s.Name}
-	}
-	a = &Tanh{}
-	return nil
-}
-
 // Source for linear tanh activation function: http://leon.bottou.org/slides/tricks/tricks.pdf
 
 // LinearTahn is the Tanh activation function plus a small linear term (set to 0.01).
@@ -225,26 +136,4 @@ func (a LinearTanh) Activate(sum float64) float64 {
 // with respect to the weighted sum
 func (a LinearTanh) DActivateDCombination(sum, output float64) float64 {
 	return TanhDerivConst*(1.0-math.Tanh(TwoThirds*sum)*math.Tanh(TwoThirds*sum)) + 0.01
-}
-
-func (a LinearTanh) String() string {
-	return linearTanhString
-}
-
-var linearTanhMarshalString string = prefix + linearTanhString
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a LinearTanh) MarshalJSON() ([]byte, error) {
-	return json.Marshal(marshalName{Name: linearTanhMarshalString})
-}
-
-// MarshalJSON marshalls the sigmoid into UTF-8 text
-func (a *LinearTanh) UnmarshalJSON(input []byte) error {
-	s := &marshalName{}
-	json.Unmarshal(input, &s)
-	if s.Name != linearTanhMarshalString {
-		return common.UnmarshalMismatch{Expected: linearTanhMarshalString, Received: s.Name}
-	}
-	a = &LinearTanh{}
-	return nil
 }
